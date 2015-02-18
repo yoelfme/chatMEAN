395cb95fc6cd57240403363ef5014ad13539014e
Yoel Monzón <yoelfme@hotmail.com>
2015-02-17 23:29:54 -0600

    Configure structure

---
+++ models/index.js
@@ -0,0 +1,14 @@
+if(!global.hasOwnProperty('db')) {
+	var mongoose = require('mongoose');
+	var dbName = 'chatMean';
+
+	mongoose.connect('mongodb://localhost/' + dbName);
+
+	global.db - {
+		mongoose: mongoose,
+		User: require('./User')(mongoose),
+		Message: require('./Message')(mongoose)
+	}
+}
+
+module.exports = global.db;
