587082d0c85052d17aaa56575800f535a5a8b680
Yoel Monzón <yoelfme@hotmail.com>
2015-03-14 23:37:45 -0600

    [update] Configure logout and login.

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -1,4 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+/* Middleware - Filters */
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+
+router.get('/', restrict, function (req, res, next) {
+	res.render('chat',{
+		title:'Chat'
+	});
+});
+
 module.exports = router;
--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+app.post('/login', function(req, res){
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      req.session.regenerate(function(){
+        req.session.user = user;
+        res.redirect('back');
+      });
+    } else {
+      res.redirect('login');
+    }
+  });
+});
+
 /* Routes get of home */
-router.get('/', function  (req, res, next) {
+router.get('/',function (req, res, next) {
+	res.redirect('/chat');
+})
+
+router.get('/login', function  (req, res, next) {
 	res.render('login',{
 		title:'Home'
 	});
 });
+
+app.get('/logout', function (req, res) {
+   delete req.session.user_id;
+   res.redirect('/login');
+});  
+
 
 router.get('/register', function (req, res, next) {
 	res.render('register',{
@@ -26,9 +49,19 @@ route.post('/register', function (req, res, next) {
 
 		res.redirect('/login');
 	});
-})
-
-
-
+});
 
 module.exports = router;
+
+
+// Functions helpers
+function authenticate(name, pass, fn) {
+  db.User.findOne ({username: name}, function(err, user) {
+    if (!user) return fn(new Error('cannot find user'));
+    hash(pass, user.salt, function(err, hash){
+      if (err) return fn(err);
+      if (hash == user.hash) return fn(null, user);
+      fn(new Error('invalid password'));
+    })
+  })
+}
